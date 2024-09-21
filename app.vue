ad3aedb366fbb8554bcfefb6aeaccf59025de904
Felix <schreibmiraufdisc@dulappen.at>
2024-09-21 19:33:31 +0200

    worked on layout of home, added contact card

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="mx-auto my-lg-3" style="max-width: 1000px;"> 
+  <div class="mx-auto my-lg-3" style="max-width: 1000px">
     <Navbar />
-    <NuxtPage class="px-lg-3 py-lg-3 py-1"/>
+    <NuxtPage class="px-3 py-3" />
   </div>
 </template>
 
@@ -12,9 +12,16 @@
 .bg-placeholder {
   background-color: #b6b6b6;
   opacity: 0.8;
-  background-image:  radial-gradient(#fafafa 0.5px, transparent 0.5px), radial-gradient(#fafafa 0.5px, #b6b6b6 0.5px), repeating-linear-gradient( -45deg, #fafafa, #fafafa 5px, #b6b6b6 5px, #b6b6b6 25px );;
+  background-image: radial-gradient(#fafafa 0.5px, transparent 0.5px),
+    radial-gradient(#fafafa 0.5px, #b6b6b6 0.5px),
+    repeating-linear-gradient(
+      -45deg,
+      #fafafa,
+      #fafafa 5px,
+      #b6b6b6 5px,
+      #b6b6b6 25px
+    );
   background-size: 20px 20px;
-  background-position: 0 0,10px 10px;
+  background-position: 0 0, 10px 10px;
 }
-
 </style>
--- components/navbar.vue
+++ components/navbar.vue
@@ -1,5 +1,6 @@
 <template>
-  <nav class="navbar navbar-expand-lg rounded navbar-sticky ">
+  <div class="sticky-top">
+    <nav class="navbar navbar-expand-lg bg-body rounded">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Navbar</a>
       <button
@@ -16,7 +17,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <NuxtLink to="/" class="nav-link">Home</NuxtLink>
+            <NuxtLink to="/" class="nav-link">Home <i class="bi bi-house-fill"></i></NuxtLink>
           </li>
           <li class="nav-item">
             <NuxtLink to="/team" class="nav-link">Team</NuxtLink>
@@ -25,7 +26,7 @@
             <NuxtLink to="/ordination" class="nav-link">Ordination</NuxtLink>
           </li> 
           <li class="nav-item">
-            <NuxtLink to="/kontakt" class="nav-link">Kontakt</NuxtLink>
+            <NuxtLink to="/kontakt" class="nav-link">Kontakt <i class="bi bi-telephone-fill"></i></NuxtLink>
           </li>
         </ul>
         <form class="d-flex" role="search">
@@ -35,4 +36,33 @@
     </div>
   </nav>
   <div class="border-bottom"></div>
+  </div>
 </template>
+
+<style>
+.nav-link {
+  width: fit-content;
+	color: #18272F;
+  position: relative;
+  text-decoration: none;
+}
+
+.nav-link::before {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 1px;
+  border-radius: 1px;
+  background-color: #18272F;
+  bottom: 0;
+  left: 0;
+  transform-origin: right;
+  transform: scaleX(0);
+  transition: transform .3s ease-in-out;
+}
+
+.nav-link:hover::before {
+  transform-origin: left;
+  transform: scaleX(1);
+}
+</style>
