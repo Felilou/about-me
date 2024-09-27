fa75ced725387bccfc74c96c819a99f6dbd6b596
Felilou <104273070+Felilou@users.noreply.github.com>
2024-09-27 07:13:27 +0000

    Build: (adfa85a) Added footer, made more accessable removed placeholder

--- _nuxt/B8dO_0Vu.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./BLgYwXfJ.js";import{_ as c,c as n,a as s,b as a,o as r}from"./Badin_Gf.js";const _={},i={class:"container-fluid p-0"},d={class:"row"},l={class:"col"};function m(f,e){const o=t;return r(),n("div",null,[e[0]||(e[0]=s("h1",{class:"mb-2"},"Ene Königswieser",-1)),s("div",i,[s("div",d,[s("div",l,[a(o)])])])])}const v=c(_,[["render",m]]);export{v as default};
