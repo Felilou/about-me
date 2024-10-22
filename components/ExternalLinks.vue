b4bd2088ba44f6e40919246c1b04628f19f5db7d
felix huber <felilou@aon.at>
2024-10-22 11:17:02 +0200

    tradawda

---
+++ components/ExternalLinks.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const links = [
+{
+  icon: 'i-heroicons-home',
+  label: '',
+  to:'https://mundhygiene-zahnarzt.at/'
+},
+{
+  icon: 'i-mdi-facebook-box',
+  label: '',
+  to:'https://www.facebook.com/'
+}, {
+  icon: 'i-mdi-instagram',
+  label: '',
+  to: 'https://www.instagram.com/'
+}]
+</script>
+
+<template>
+  <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800 justify-center" />
+</template>
+
--- components/footer.vue
+++ components/footer.vue
@@ -1,17 +1,22 @@
 <template>
- 
-  <div class="flex flex-col bg-white w-screen">
-    <UDivider icon="i-heroicons-information-circle-solid"/>
-    <div class="flex m-0 p-2 flex-col lg:flex-row items-stretch">
-      <div class="p-2 lg:w-[50%]">
+
+  <div class="flex flex-col bg-white w-screen p-2">
+    <UDivider icon="i-heroicons-information-circle-solid" />
+    <div class="flex m-0 flex-col lg:flex-row items-stretch">
+      <div class="p-1 lg:w-[50%]">
         <OpenTimesTable />
       </div>
-      <div class="p-2 lg:w-[50%]">
-        <ContactCard /> 
+      <div class="p-1 lg:w-[50%]">
+        <ContactCard />
       </div>
-  </div>
-  <UDivider label="@"/>
-  
+    </div>
+    <UDivider icon="i-heroicons-arrow-top-right-on-square-16-solid" />
+    <div>
+      <ExternalLinks />
+    </div>
+    <div>
+      Lesen sie mehr <a href="" class="text-primary underline">hier</a>
+    </div>
   </div>
 </template>
 
