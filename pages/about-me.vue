aa3e9cb9f36fc4a65130159bab662d87855b7ac2
Felix <HUB210654@Spengergasse.at>
2025-01-18 15:00:23 +0100

    waas

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -123,6 +123,21 @@
 </template>
 
 <script setup>
+useSeoMeta({
+  title: 'Ene Königswieser | Mikroskop - Zahnarztpraxis',
+  ogTitle: 'Ene Königswieser | Mikroskop - Zahnarztpraxis',
+  description: 'Dr.Ene Königswieser. Präzise Zahnbehandlung mit langanhaltenden Ergebnissen, in freundlicher Athmosphäre.',
+  ogDescription: 'Dr.Ene Königswieser. Präzise Zahnbehandlung mit langanhaltenden Ergebnissen, in freundlicher Athmosphäre.',
+  ogImage: 'https://raw.githubusercontent.com/Felilou/about-me/refs/heads/build/img/20240916-GZ1A8139.jpg',
+  colorScheme: 'only light',
+  themeColor: 'blue',
+  applicationName: 'Ene Königswieser about me',
+  author: 'Ene Königswieser',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  robots: 'index, follow',
+  keywords: 'Ene Königswieser, Zahnarzt, Zahnarztpraxis, Mikroskop, Zahnarzt Praxis, Zahnarzt Praxis Wien, Zahnarzt Praxis in Wien, Zahnarzt Praxis in 1010 Wien, Zahnarzt Praxis in 1010, Zahnarzt Praxis in Wien, Zahnarzt Praxis in 1010 Wien, Zahnarzt Praxis in 1010 Wien, Zahnarzt Praxis in 1010 Wien, Zahnarzt Praxis in 1010 Wien, Zahnarzt Praxis in 1010 Wien, Zahnarzt Praxis in 1010 Wien, Zahnarzt Praxis in 1010 Wien, Zahnarzt Praxis in 1010 Wien, Zahnarzt Praxis in 1010 Wien',
+  ogType: 'website',
+})
 </script>
 
 <style scoped>
