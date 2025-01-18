6cca47a6fbe64930167ccc3384979b4b26b93141
Felix <HUB210654@Spengergasse.at>
2025-01-18 14:55:38 +0100

    wasa

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLayout>
+    <slot />
+  </NuxtLayout>
+</template>
+
+<script setup>
+useHead({
+  title: 'Nuxt 3 + Tailwind CSS + UI',
+  meta: [
+    { name: 'description', content: 'Nuxt 3 + Tailwind CSS + UI' },
+    { name: 'keywords', content: 'Nuxt 3, Tailwind CSS, UI' },
+    { name: 'author', content: 'Your Name' },
+  ]
+});
+</script>
